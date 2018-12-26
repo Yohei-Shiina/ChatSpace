1256133687b16c8661410d057f1f570952f04475
EC2 Default User <ec2-user@ip-172-31-9-180.ap-southeast-1.compute.internal>
2018-12-26 15:48:27 +0000

    set flash message
    create _notifications.html.erb
    
    cretet and set devise.ja.yml file to japanize flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users
   root 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:new, :create ,:edit, :update]
   
-  
-  
+
 end
