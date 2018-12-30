375457327204e50e73b56da9fa8e0372d526d893
EC2 Default User <ec2-user@ip-172-31-9-180.ap-southeast-1.compute.internal>
2018-12-30 08:26:40 +0000

    make index file in view/groups
    edit view/groups/new file add collection_chat_boxes
    
    make partial file in view/messages/index

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
-  
-  has_many :users, through: :gourp_users
+
+  has_many :users, through: :group_users
   has_many :group_users
+
+  validates_presence_of :name
 end
