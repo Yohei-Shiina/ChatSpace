28a20d461429cdbaab2bf6e1a25e709096c9d03c
EC2 Default User <ec2-user@ip-172-31-9-180.ap-southeast-1.compute.internal>
2019-02-19 07:01:50 +0000

    create app/controllers/messages_controller.rb,
    create file /home/ec2-user/environment/chat-space/app/assets/javascripts/message.js

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,9 @@ class MessagesController < ApplicationController
   
   def create
     @message = @group.messages.new(message_params)
+    respond_to do |format|
+      format.html #{ redirect_to group_messages_path(params[:group_id])}
+      format.json
  
     
     if @message.save
