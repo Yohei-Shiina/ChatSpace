6773122f27cd4e6a110ea0557cd9176dd5f8b8d7
EC2 Default User <ec2-user@ip-172-31-9-180.ap-southeast-1.compute.internal>
2019-02-22 08:28:27 +0000

    change notice message in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     
     if @message.save
       respond_to do |format|
-      format.html { redirect_to group_messages_path(@group), notice: "HTMLが呼ばれた"}
+      format.html { redirect_to group_messages_path(@group), notice: "メッセージが送信されました"}
       format.json 
     end
     
